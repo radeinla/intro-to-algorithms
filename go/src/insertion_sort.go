506c778b4f07023e789489efe3b82d3d9966e3e3
R. Deinla <radeinla@gmail.com>
2019-06-01 19:39:47 +0700

    Add a bit more cases to make sure it works.

--- go/src/insertion_sort.go
+++ go/src/insertion_sort.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func insertionSort(arr []int) []int {
 	fmt.Printf("%v\n", arr)
+
 	for i := 1; i < len(arr); i++ {
 		for j := i - 1; j >= 0; j-- {
 			if arr[j+1] < arr[j] {
@@ -12,10 +13,16 @@ func insertionSort(arr []int) []int {
 			fmt.Printf("%v\n", arr)
 		}
 	}
+
 	return arr
 }
 
 func main() {
-	arr := []int{5, 2, 4, 6, 1 ,3};
-	fmt.Printf("%v\n", insertionSort(arr))
+	fmt.Printf("%v\n", insertionSort([]int{5, 2, 4, 6, 1 ,3}))
+
+	fmt.Printf("%v\n", insertionSort([]int{2, 1}))
+
+	fmt.Printf("%v\n", insertionSort([]int{1}))
+
+	fmt.Printf("%v\n", insertionSort([]int{}))
 }
