cac17387be0660fe9007456949073fa583a74bc3
R. Deinla <radeinla@gmail.com>
2019-06-01 19:31:46 +0700

    Add insertion sort implementation in go

---
+++ go/src/insertion_sort.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func insertionSort(arr []int) []int {
+	fmt.Printf("%v\n", arr)
+	for i := 1; i < len(arr); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if arr[j+1] < arr[j] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+			}
+			fmt.Printf("%v\n", arr)
+		}
+	}
+	return arr
+}
+
+func main() {
+	arr := []int{5, 2, 4, 6, 1 ,3};
+	fmt.Printf("%v\n", insertionSort(arr))
+}
